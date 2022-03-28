db94795667537189190196058ece5aabd2e1b231
boobalannov <boobalanannamalai25@gmail.com>
2022-03-28 22:14:14 +0530

    new project added

---
+++ src/aadhi/example.java
@@ -0,0 +1,18 @@
+package aadhi;
+
+class example {
+	public static void main(String[] args) {
+		//byte
+		//short
+		// int
+		//long
+		//float
+		//double
+		//char
+		//boolean
+		String myString="this is string";
+		System.out.println("myString is equal to" + myString);
+		
+		
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module aadhi.java {
+}
